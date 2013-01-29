e5945f8b59cef8ebd41aa65f587d42351a26e8f9
Kevin Smith <kebm@hdlnerd.com>
2013-01-29 00:17:57 -0600

    petsupervisor now equivalent to 1/19 version of cathy's critters, including the database.

--- dump_sql.sh
+++ dump_sql.sh
@@ -4,5 +4,5 @@
 # merge the dump files and allow us to integrate the two databases?
 #
 
-mysqldump -u critters_kevin -p711coffeesql --skip-extended-insert critters_petsupervisor > ~/sites/cathys-critters.com/critters_critterdb.sql
+mysqldump -u critters_kevin -p711coffeesql --skip-extended-insert critters_petsupervisor > ~/sites/petsupervisor.com/critters_critterdb.sql
 
