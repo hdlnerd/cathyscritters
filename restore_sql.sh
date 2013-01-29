141bf42acdfc59bc1edd6c02ac2af1d418a8aaad
Kevin Smith <kebm@hdlnerd.com>
2013-01-29 00:47:29 -0600

    True clone of 1/19 cathys-critters database

--- restore_sql.sh
+++ restore_sql.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysql -u critters_kevin -p711coffeesql critters_petsupervisor < ~/sites/cathys-critters.com/critters_critterdb.sql
+mysql -u critters_kevin -p711coffeesql critters_petsupervisor < ~/sites/petsupervisor.com/critters_critterdb.sql
