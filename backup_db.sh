91b2598365bcbb19d0f0664cbfa730c670acc845
Kevin Smith <kebm@hdlnerd.com>
2013-01-31 11:36:33 -0600

    Removed scripts

--- backup_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Dump the database before every commit.  Then, merging a branch should
-# merge the dump files and allow us to integrate the two databases?
-#
-
-mysqldump -u critters_kevin -p711coffeesql --skip-extended-insert critters_critterdb > ~/sites/cathyscritters.com/critters_critterdb_bak.sql
-
